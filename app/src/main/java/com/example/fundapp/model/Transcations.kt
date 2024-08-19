8c2681d4f44458b25293776b5911c5a0c00ab784
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-08-19 17:39:37 +0500

    working on Deposit

--- app/src/main/java/com/example/fundapp/model/Transcations.kt
+++ app/src/main/java/com/example/fundapp/model/Transcations.kt
@@ -1,6 +1,13 @@
 package com.example.fundapp.model
- class Transcations(
-
-
-
+data class Transaction(
+    val transactionId: String,
+    val userId: String,
+    val type: String, // "deposit" or "withdrawal"
+    val amount: Double,
+    val date: String,
+    val proofOfDeposit: String? = null,
+    val comments: String? = null,
+    val reason: String? = null,
+    val status: String = "pending" // "pending", "accepted", "rejected"
 )
+
