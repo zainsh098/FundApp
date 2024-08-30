9a6d63f4c07c79ff52348512a2f1cb1c3bea7b85
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-08-30 12:27:06 +0500

    status bar color changes

--- app/src/main/java/com/example/fundapp/MainActivity.kt
+++ app/src/main/java/com/example/fundapp/MainActivity.kt
@@ -14,8 +14,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(binding.root)
+        window.statusBarColor = getResources().getColor(R.color.Violet_100,);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
